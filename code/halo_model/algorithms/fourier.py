3ef379be66384157b2b99f74f4490d3bf4b13a50
Milan Tol <milantol35@gmail.com>
2026-06-08 13:36:43 +0200

    .

--- code/halo_model/algorithms/fourier.py
+++ code/halo_model/algorithms/fourier.py
@@ -2,17 +2,7 @@ import numpy as np
 from scipy.integrate import romb
 import mcfit
 
-# def fourier(k, rho_r:callable, rmin, rmax, N_r=2**7):
-#     k = k 
-#     def integrand(lnr):
-#         r = np.exp(lnr)
-#         return 4*np.pi * r*r * np.sinc(k*r / np.pi) * rho_r(r) * r # add factor r for conversion dr -> dr/dlnr dlnr
-    
-#     lnrs = np.linspace(np.log(rmin), np.log(rmax), N_r)
-#     dlnr = lnrs[1] - lnrs[0]
-#     ys = [integrand(lnr) for lnr in lnrs]
-    
-#     return romb(ys, dlnr) 
+import warnings
 
 def fourier(
     rho_r:callable, rmin:float, rmax:float, Nr:int, q:float=1.5
@@ -24,8 +14,10 @@ def fourier(
     f_r = rho_r(r_grid)
     # don't pass q=0 — use default q=1.5
     # use lowring=True and extrap=True
-    transform = mcfit.SphericalBessel(r_grid, q=q, lowring=True)
-    k, Fk = transform(f_r)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")   # suppress jax warning
+        transform = mcfit.SphericalBessel(r_grid, q=q, lowring=True)
+        k, Fk = transform(f_r)
     # correct normalization factor
     Fk = Fk / Fk[0]   
       
